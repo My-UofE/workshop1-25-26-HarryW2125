b40383dd8c912ae602fd88f9b3bfaff08118a07a
HarryW2125 <hjw235@exeter.ac.uk>
2026-01-16 13:15:56 +0000

    updated process guess function

--- game_functions.py
+++ game_functions.py
@@ -5,13 +5,24 @@ def pick_value(poss_values):
     x = random.choice(poss_values)   
     return x
 
-# function to be used in game_2: Higher or Lower
-def check_higher_lower(current_val, next_val, user_input):
-    pass
-
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
-    pass
+    temp=[]
+    for i in range(len(word)):
+        if word[i] == letter:
+            board[i] = letter
+            temp.append(i)
+    
+    if len(temp) > 0:
+        print("Well done!", letter,"is in the word")
+        return True
+    else:
+        print("sorry,", letter,"is not in the word")
+        return False
+    
+
+
+
 
 def check_higher_lower(current_val, next_val, user_input):
     if current_val > next_val:
